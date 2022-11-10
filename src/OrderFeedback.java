d2e004598eb472783fc5d12e0601d978d9613c2a
ErmakovRS <rermakov777@gmail.com>
2022-11-10 14:07:26 +0300

    new Class new Method

---
+++ src/OrderFeedback.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class OrderFeedback {
+    public void feedback() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println(" Оставьте отзыв!!!");
+        String feedback = scanner.next();
+        System.out.println(" Спасибо!!!");
+
+    }
+}
